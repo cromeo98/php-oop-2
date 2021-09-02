0cbf8718de9ca713ae62ffaa124adc595f112487
cromeo98 <romeo.christian98@gmail.com>
2021-09-02 17:08:13 +0200

    init

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
+// Ciao ragazzi, esercizio di oggi: nome repo: php-oop-2
+// Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping. 
+// Strutturare le classi gestendo l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+// Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello shop inserisce una carta di credito... $c = new CreditCard(..); $user->insertCreditCard($c);
+// BONUS: Gestite eventuali eccezioni che si possono verificare (es: carta di credito scaduta).
 
+
+
+// CLASSI:
+// CHI EFFETTUA ACCESSO SUL SITO? 
+// -> CLIENTI
+// -> AMMINISTRATORI
+
+// --> GESTIRE LE UTENZE
+
+// COSA SI PUò TROVARE SUL SITO?
+// prodotti --> PRODOTTI IN OFFERTA
+
+// I CLIENTI POTREBBERO AVERE UNO SCONTO DEL 5% MENTRE GLLI UTENTI CHE ACQUISTANO SENZA REGISTRAZIONE NO
+
+// CREARE DB:
+
+// UTENTI REGISTRATI
+
+
+
+// prodotti
+// utenti
